546f696f21b45a39b53025bc61ae938a52ea6533
Aawesh Manyar <awezmanyar1672@gmail.com>
2023-09-11 13:12:59 +0530

    secondcommit

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
 const { ExpressPeerServer } = require("peer");
 const opinions = {
   debug: true,
-};
-
-app.set("view engine", "ejs");
-
-app.use(express.static("public"));
+}
 
 app.use("/peerjs", ExpressPeerServer(server, opinions));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -21,18 +24,12 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: '*'
-  }
-});
-
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-    setTimeout(() => {
+    setTimeout(()=>{
       socket.to(roomId).broadcast.emit("user-connected", userId);
-    }, 1000);
+    }, 1000)
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
     });
